e7452758b4940198e879e9ca3cdf89751d3f3e98
Lenny Mo <lennymo.work@gmail.com>
2023-12-30 00:21:38 +0800

    update ratelimit

--- middleware/ratelimit.go
+++ middleware/ratelimit.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -8,68 +9,70 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type rateLimiter struct {
+	channel chan struct{}
+	once    sync.Once // 限制开启漏水口
+}
+
 // 创建两个 channel
 var (
 	// 每个路由组都有自己的单独限流器, 路由组name作为key, channel 作为value
 	ratelimitMap = sync.Map{}
-	// 存储每个限流器对应的漏水口, value 是一个sync.Once对象
-	leakDropletMap = sync.Map{}
 )
 
 // RateLimit
-func RateLimit(chanName string, capacity int64) func(c *gin.Context) {
-	// 1. 创建一个channel
-	createChanByName(chanName, capacity)
-	// 2. 开启一个出水口
-	perRequest := time.Second / time.Duration(capacity) // 计算每个request的耗时
-	fmt.Println("perRequest: ", perRequest)
-	getOutOfMyBucket(chanName, perRequest)
+//
+// ctx 用来控制漏水口的结束
+//
+// chanName 指定队列的名称，capacity 指定队列长度，1秒内最多可以承受的最大并发数
+func RateLimit(ctx context.Context, chanName string, capacity int64) func(c *gin.Context) {
+	// 1. 创建一个限流队列，并且开启一个对应的出水口
+	// 如果这个键存在，就返回已存在的值；如果这个键不存在，就存储一个新的值（新创建的带有指定容量的通道）
+	rateLimiterIface, loaded := ratelimitMap.LoadOrStore(chanName, &rateLimiter{
+		channel: make(chan struct{}, capacity),
+	})
+	limiter := rateLimiterIface.(*rateLimiter) // 指针类型可以直接修改结构体内部的值，而不需要进行值的拷贝
+	if !loaded {
+		perRequest := time.Second * 10 / time.Duration(capacity) // 每个请求的间隔
+		fmt.Println("每个请求的间隔: ", perRequest)
+		limiter.once.Do(func() {
+			fmt.Println("开启排水功能：", chanName)
+			go getOutOfMyBucket(ctx, chanName, limiter.channel, perRequest) // 2. 开启一个出水口
+		})
+	}
+
 	// 3. 放入请求
 	return func(c *gin.Context) {
 		// 根据名字获取到对应的通道
 		value, _ := ratelimitMap.Load(chanName)
-		ch := value.(chan struct{})
+		ch := value.(*rateLimiter).channel
 
 		select {
 		case ch <- struct{}{}:
 			// 如果能在限时时间内写入则 继续执行
 			c.Next()
-		case <-time.After(500 * time.Millisecond):
+		case <-time.After(200 * time.Millisecond):
 			// 如果时间到了，则直接返回限流信息
 			c.AbortWithStatusJSON(429, gin.H{"message": "Too Many Requests"})
 		}
 	}
 }
 
-// createChanByName 根据名字创建对应的限流队列
-func createChanByName(name string, capacity int64) {
-	// 判断不存在才创建
-	if _, ok := ratelimitMap.Load(name); !ok {
-		ratelimitMap.Store(name, make(chan struct{}, capacity))
+// getOutOfMyBucket 定时排出队列中的水
+func getOutOfMyBucket(ctx context.Context, name string, c <-chan struct{}, perRequest time.Duration) {
+	// 开启一个goroutine 执行漏水任务
+	for {
+		select {
+		case <-ctx.Done():
+			// 父coroutine退出
+			fmt.Println("漏水任务安全退出: ", name)
+			return
+		default:
+			// 定时排水任务
+			<-time.After(perRequest)
+			<-c
+			fmt.Println("排水成功", "队列容量：", len(c))
+		}
 	}
-}
 
-// getOutOfMyBucket 根据名字创建一个单例函数，根据perRequest 设置流水速度
-// 根据名字获取到对应的队列，定时排出队列中的水
-func getOutOfMyBucket(name string, perRequest time.Duration) {
-	// 1. 如果不存在则创建once对象
-	if _, ok := leakDropletMap.Load(name); !ok {
-		leakDropletMap.Store(name, &sync.Once{})
-	}
-	// 2. 断言
-	value, _ := leakDropletMap.Load(name)
-	value.(*sync.Once).Do(func() {
-		// 3. 根据名字获取到队列
-		fmt.Println("漏水口开始执行：", name)
-		c, _ := ratelimitMap.Load(name)
-		queue := c.(chan struct{})
-		// 开启一个goroutine 执行漏水任务
-		go func() {
-			for {
-				<-time.After(perRequest) // 出水速度：等待10毫秒
-				<-queue
-				fmt.Println("排水成功")
-			}
-		}()
-	})
 }
