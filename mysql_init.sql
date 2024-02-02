f67cc95942af6766bf8eecd3d593a5fd8beecbe5
Lenny Mo <lennymo.work@gmail.com>
2024-02-02 12:25:18 +0800

    update sql

--- mysql_init.sql
+++ mysql_init.sql
@@ -1,49 +1,41 @@
 create database if not exists test;
 use test;
 
-drop table if exists `user`;
-create table `user` (
-                        `id` bigint(20) not null auto_increment,
-                        `user_id` bigint(20) not null,
-                        `name` varchar(64) not null,
-                        `password` varchar(256) not null,
-                        `email` varchar(64) not null,
-                        `gender` tinyint(4) not null,
-                        `create_at` timestamp null default current_timestamp,   -- 默认使用当前时间戳
-                        `update_at` timestamp null default current_timestamp
-                            on update current_timestamp,    -- 每次更新的时候使用当前时间戳
-                        primary key (`id`),
-                        unique key `index_user_id` (`user_id`),
-                        unique key `index_name` (`name`)
-) engine=innoDB default charset=utf8mb4 collate=utf8mb4_general_ci;
+drop table if exists user;
+CREATE TABLE user (
+                      id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+                      user_id BIGINT NOT NULL UNIQUE,
+                      name VARCHAR(64) NOT NULL UNIQUE,
+                      password VARCHAR(256) NOT NULL,
+                      email VARCHAR(64) NOT NULL,
+                      gender TINYINT NOT NULL,
+                      create_at TIMESTAMP NOT NULL,
+                      update_at TIMESTAMP NOT NULL
+)engine=innoDB default charset=utf8mb4 collate=utf8mb4_general_ci;
 
 
--- 创建一个账户表
-drop table if exists `community`;
-create table `community` (
-                             `id` int(11) unsigned not null auto_increment,
-                             `community_id` bigint(20) unsigned not null,
-                             `community_name` varchar(64) not null collate utf8mb4_general_ci,
-                             `community_intro` varchar(256) not null collate utf8mb4_general_ci,
-                             `create_at` timestamp not null default current_timestamp,
-                             `update_at` timestamp not null default current_timestamp on update current_timestamp,
-                             primary key (`id`),
-                             unique key `index_community_id` (`community_id`),
-                             unique key `index_community_name` (`community_name`)
-) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_general_ci;
+drop table if exists community;
+CREATE TABLE community (
+                           id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                           community_id BIGINT UNSIGNED NOT NULL UNIQUE,
+                           community_name VARCHAR(64) NOT NULL UNIQUE,
+                           community_intro VARCHAR(256) NOT NULL,
+                           create_at TIMESTAMP NOT NULL,
+                           update_at TIMESTAMP NOT NULL
+)engine=innoDB default charset=utf8mb4 collate=utf8mb4_general_ci;
 
 -- 创建一个post表
-drop table if exists `post`;
-create table `post` (
-                        `id` bigint(20) unsigned not null auto_increment,
-                        `post_id` bigint(20) unsigned not null,
-                        `title` varchar(256) not null,
-                        `content` tinytext not null,
-                        `author_id` bigint(20) unsigned not null,
-                        `community_id` bigint(20) unsigned not null,
-                        `status` tinyint(4) not null default 0,
-                        `create_at` timestamp not null default current_timestamp,
-                        `update_at` timestamp not null default current_timestamp on update current_timestamp,
-                        primary key (`id`),
-                        unique key `index_post_id` (`post_id`)
-) engine =InnoDB default charset=utf8mb4 collate=utf8mb4_general_ci;
+drop table if exists post;
+CREATE TABLE post (
+                      id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                      post_id BIGINT UNSIGNED NOT NULL UNIQUE,
+                      title VARCHAR(256) NOT NULL,
+                      content TINYTEXT NOT NULL,
+                      author_id BIGINT UNSIGNED NOT NULL,
+                      community_id BIGINT UNSIGNED NOT NULL,
+                      status TINYINT NOT NULL DEFAULT 0 COMMENT '',
+                      create_at TIMESTAMP NOT NULL,
+                      update_at TIMESTAMP NOT NULL,
+                      INDEX idx_community_id (community_id),
+                      FOREIGN KEY (community_id) REFERENCES community(community_id) ON DELETE CASCADE
+)engine=innoDB default charset=utf8mb4 collate=utf8mb4_general_ci;
